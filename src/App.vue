09323d5a1c16a574850e067b69c043a2c44ffa7e
Fakhar Abbas <afakhar363@gmail.com>
2023-02-15 00:16:26 +0500

    The NavBar is added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
+  <the-navbar></the-navbar>
   <router-view />
 </template>
 
 <script>
+import TheNavbar from "./components/TheNavbar.vue";
+export default {
+  components: { TheNavbar },
+};
 //
 </script>
